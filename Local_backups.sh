4bda38a257ac3459f9e4233644fd8af299400f38
Daniel Rosehill <40233203+danielrosehilljlm@users.noreply.github.com>
2019-10-26 21:52:39 +0300

    Former local file backup
    For crontab. Local file backup including:
    
    -> LibreOffice user dictionary
    -> Scripts directory
    -> Openbox config
    -> Autokey phrases and hotkeys
    -> All desktop launchers (make sure /home/user/Desktop is not set as the default download directory!)

---
+++ Local_backups.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp -r /home/daniel/.config/libreoffice/4/user ~/pCloudDrive/Technical/Backups/Automatic/Alma63Desktop &
+cp -a /home/daniel/Scripts ~/pCloudDrive/Technical/Backups/Automatic/Alma63Desktop &
+cp /home/daniel/.config/openbox/lubuntu-rc.xml  ~/pCloudDrive/Technical/Backups/Automatic/Alma63Desktop &
+cp -r  /home/daniel/.config/autokey ~/pCloudDrive/Technical/Backups/Automatic/Alma63Desktop &
+# cp -r  /home/daniel/Desktop/* ~/pCloudDrive/Technical/Backups/Automatic/Alma63Desktop &
+exit
