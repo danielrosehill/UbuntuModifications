64574ad58e54b8b075b9702ed856ed9d64d53caf
Daniel Rosehill <daniel@danielrosehill.co.il>
2019-10-28 18:33:08 +0200

    Started the program installing script

---
+++ Scripts/Master_Packet_installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Bash script to install all programs, PPAs, and some masic modifications. No local order. Work in progress! #
+sudo apt-get install -y emacs &
+sudo snap install -y spotify &
+sudo apt-get install -y redshift &
+sudo apt-get install -y redshift-gtk
+
+
+Exit
